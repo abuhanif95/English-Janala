35e771a024f553afc071b842bee3ee564c93a6a7
Hanif <mohammadabuhanif95@gmail.com>
2026-03-05 18:56:17 +0800

    add search section and complete the project

--- script/script.js
+++ script/script.js
@@ -150,3 +150,21 @@ const displayLesson = (lessons) => {
 };
 
 loadLesson();
+
+document.getElementById("btn-search").addEventListener("click", () => {
+  removeActive();
+
+  const input = document.getElementById("input-search");
+  const searchValue = input.value.trim().toLowerCase();
+  // console.log(searchValue)
+
+  fetch("https://openapi.programming-hero.com/api/words/all")
+    .then((response) => response.json())
+    .then((data) => {
+      const allWords = data.data;
+      const filterWords = allWords.filter((word) =>
+        word.word.toLowerCase().includes(searchValue),
+      );
+      displayLevelWord(filterWords);
+    });
+});
